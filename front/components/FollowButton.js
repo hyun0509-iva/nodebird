e6d376144eb1e599c0462870aca239786d871d91
Dhyunlee <skyblue5030@naver.com>
2023-05-18 16:27:14 +0900

    게시글의 작성자가 로그인 한 유저인 경우 팔로우 버튼 보이지 않게 처리

--- front/components/FollowButton.js
+++ front/components/FollowButton.js
@@ -24,6 +24,10 @@ const FollowButton = ({ post }) => {
     }
   }, [isFollowing]);
 
+  if (post.User.id === me.id) {
+    return null;
+  }
+  
   return (
     <Button loading={followLoading || unFollowLoading} onClick={onFollow}>
       {isFollowing ? '언팔로우' : '팔로우'}
